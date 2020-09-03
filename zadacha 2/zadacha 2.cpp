ac20884f56e8c56bfcb5d1d46c046bc9754f93ab
GSLazarov18 <GSLazarov18@codingburgas.bg>
2020-09-03 11:21:17 +0300

    trqbva da napisha neshto

--- zadacha 2/zadacha 2.cpp
+++ zadacha 2/zadacha 2.cpp
@@ -1,12 +1,22 @@
-
 #include <iostream>
-#include <iomanip>
 #include <string>
 using namespace std;
 int main()
 {
-	//pravq repositori v github 
+	int arr[20];
+	int broi = 0;
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> arr[i];
 
+		if (i > 0 && arr[i] % i != 0)
+		{
+			cout << arr[i] << " ";
+			broi++;
+		}
+	}
+
+	cout << endl << broi;
 }
-
-
